547fb27bdfd831f4899094c87af0e6ebf79c6073
khadim2000 <kdiop8529@gmail.com>
2024-04-19 01:43:21 +0200

    first commit

---
+++ khadim-laravel/app/Models/Serive_Financiere.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Serive_Financiere extends Model
+{
+    use HasFactory;
+}
