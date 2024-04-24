fb929965b848ad79e83dfd8335bd536be57bf8e6
khadim2000 <150241459+khadim2000@users.noreply.github.com>
2024-04-24 19:07:33 +0200

    Delete khadim-laravel directory

--- khadim-laravel/webpack.mix.js
+++
@@ -1,9 +0,0 @@
-// webpack.mix.js
-
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .copy('node_modules/@fortawesome/fontawesome-free/css/all.min.css', 'public/css/fontawesome.css')
-   .copy('node_modules/material-design-icons-iconfont/dist/material-design-icons.css', 'public/css/material-icons.css');
-
